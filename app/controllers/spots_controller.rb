9777f633bf42a51b0e7fd5aad38522b0d6fee294
ErikYoung <erik.young@rmit.edu.vn>
2016-12-12 04:02:33 +0700

    added simple form and changed layout

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,4 +2,19 @@ class SpotsController < ApplicationController
   def index
     @spots = Spot.all
   end
+
+  def new
+    @spot = Spot.new
+  end
+
+  def create
+    Spot.create(spot_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:name, :description, :address)
+  end
 end
