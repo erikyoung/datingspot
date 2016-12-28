08eebadca26272440cd9127ffaf3c657b28f6ceb
ErikYoung <erik.young@rmit.edu.vn>
2016-12-28 22:43:10 +0700

    adding editing to spots ability

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -12,6 +12,20 @@ class SpotsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @spot = Spot.find(params[:id])
+  end
+
+  def edit
+    @spot = Spot.find(params[:id])
+  end
+
+  def update
+    @spot = Spot.find(params[:id])
+    @spot.update_attributes(spot_params)
+    redirect_to root_path
+  end
+
   private
 
   def spot_params
